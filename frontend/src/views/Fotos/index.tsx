09c83af037356fe2d6d7e50b091edc2aff60823e
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-01-24 08:28:45 +0100

    Aula 29 - Álbum de Fotos em HTML5 - Parte 1

--- frontend/src/views/Fotos/index.tsx
+++ frontend/src/views/Fotos/index.tsx
@@ -1,25 +1,33 @@
 import Header from "components/Header";
 import PhotosIcon from 'assets/img/fotos.png';
+import ArticleHeader from "components/ArticleHeader";
 import Footer from "components/Footer";
+import './styles.css';
 
 function Fotos() {
     return (
         <div className="interface">
             <Header defaultIconMenu={PhotosIcon} />
 
-            Glass &gt; Fotos
-            Galeria de Imagens do Google Glass
-            por Gustavo Guanabara
-            Atualizado em 01/Maio/2013
+            <ArticleHeader
+                text="Glass &gt; Fotos"
+                title={"Galeria de Imagens do Google Glass"} />
 
-            Veja na nossa galeria de fotos várias belas imagens que mostram algumas das principais características do Google Glass, como recursos e propriedades que estão impressionando o mundo inteiro. Basta passar o mouse sobre uma das fotos para ver uma versão ampliada e com uma breve descrição.
+            <p>
+                Veja na nossa galeria de fotos várias belas imagens que mostram algumas
+                das principais características do Google Glass, como recursos e propriedades
+                que estão impressionando o mundo inteiro. Basta passar o mouse sobre uma das
+                fotos para ver uma versão ampliada e com uma breve descrição.
+            </p>
 
-            - Agenda e lembretes
-            - Sergey Brin usando o Glass
-            - Leve e compacto
-            - Sensação de uma tela de 50"
-            - Vários tipos de lente
-            - Informações importantes
+            <ul className="albumFotos">
+                <li id="foto01"><span>Agenda e lembretes</span></li>
+                <li id="foto02"><span>Sergey Brin usando o Glass</span></li>
+                <li id="foto03"><span>Leve e compacto</span></li>
+                <li id="foto04"><span>Sensação de uma tela de 50"</span></li>
+                <li id="foto05"><span>Vários tipos de lente</span></li>
+                <li id="foto06"><span>Informações importantes</span></li>
+            </ul>
 
             <Footer />
         </div>
